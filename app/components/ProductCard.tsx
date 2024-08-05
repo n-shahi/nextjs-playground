817bb4086f14e99c1cb5f8160e1082b6c9cacadf
NabinShahi <er.nshahi@gmail.com>
2024-08-05 22:46:22 +0900

    add client side code

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <button onClick={() => console.log('Hi there!')}>Hi There</button>
+    </div>
+  )
+}
+
+export default ProductCard
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+'use client'
 import Image from 'next/image'
+import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
   <main>
     <h1>Darsan Dad!</h1>
+    <ProductCard />
   </main>    
   )
 }
