b8f4eb5706315026eb438ef6da192a049970f413
NabinShahi <er.nshahi@gmail.com>
2024-08-08 09:01:02 +0900

    add navbar

---
+++ app/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-slate-100'>
+      <nav>
+        <ul className='flex p-5'>
+          <li><Link href="/" className='m-5'>Home</Link></li>
+          <li><Link href="/users" className='m-5'>Users</Link></li>
+          <li><Link href="/admin" className='m-5'>Admin</Link></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ app/admin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminPage = () => {
+  return (
+    <div>
+      <p>Admin Home Page</p>
+    </div>
+  )
+}
+
+export default AdminPage
