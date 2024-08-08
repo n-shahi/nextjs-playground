72f493e39a0e503760742edfa2024e266f7485a1
NabinShahi <er.nshahi@gmail.com>
2024-08-08 22:19:09 +0900

    custom not-found page

---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <span>User doesnt exist!</span>
+        </div>
+    )
+}
+
+export default NotFoundPage
--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,9 +1,13 @@
+import { notFound } from "next/navigation";
 
 interface Props {
   params: {id: number;}
 }
 
 const UserDetail = ({params: {id}}: Props) => {
+  if (id > 10) {
+    return notFound()
+  }
   return (
     <div>
       <h1>User Detail</h1>
