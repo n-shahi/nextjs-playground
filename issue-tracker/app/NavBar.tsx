f73ec3bff8797b78feeb16c9c62256dddaa6dc72
NabinShahi <er.nshahi@gmail.com>
2024-08-16 19:11:24 +0900

    refactor navbar

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -9,15 +9,23 @@ import { Avatar, Box, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 
 
 const NavBar = () => {
-  const { status, data: session } = useSession();
+  return (
+    <nav className='flex space-x-6 border-b mb-5 h-14 items-center justify-between'>
+      <NavLink />
+      <AuthStatus />
+    </nav>
+  )
+}
+
+const NavLink = () => {
   const currentPath = usePathname()
+
   const navList = [
     { name: 'Issues', href: '/issues/list' },
     { name: 'Dashboard', href: '/dashboard' },
   ]
   return (
-    <nav className='flex space-x-6 border-b mb-5 h-14 items-center justify-between'>
-      <Flex align='center' gap='2'>
+    <Flex align='center' gap='2'>
         <Link href='/'><FaBug /></Link>
         <ul>
           <li className='flex space-x-6'>
@@ -27,40 +35,40 @@ const NavBar = () => {
                 href={item.href}
                 // className={`${currentPath === item.href ? 'text-zinc-900' : 'text-zinc-500'} hover:text-zinc-800 transition-colors`}
                 className={classnames({
-                  'text-zinc-900': currentPath === item.href,
-                  'text-zinc-500': currentPath !== item.href,
-                  'border-b-2': currentPath === item.href,
-                  'hover:text-zinc-800': true,
-                  'transition-colors': true,
+                  'nav-link': true,
+                  '!text-zinc-900': currentPath === item.href,
                 })}
               >{item.name}</Link>
             )}
           </li>
         </ul>
       </Flex>
-      <Box>
-        {status === 'loading' && <div>Loading...</div>}
-        {status === 'authenticated' &&
-          // <Text>{session.user!.name} <Link href="/api/auth/signout" className='m-5'>Sign Out</Link></Text>
-          <DropdownMenu.Root>
-            <DropdownMenu.Trigger>
-              <Avatar src={session.user!.image!} fallback="?" radius='full' size='2' className='cursor-pointer'/>
-            </DropdownMenu.Trigger>
+  )
+}
 
-            <DropdownMenu.Content>
-              <DropdownMenu.Label>
-                <Text>{session.user?.email}</Text>
-              </DropdownMenu.Label>
+const AuthStatus = () => {
+  const { status, data: session } = useSession()
+  if (status === 'loading') return <div>Loading...</div>
+  if (status === 'unauthenticated') return <Link href="/api/auth/signin" className='m-5 nav-link'>Sign In</Link>
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar src={session!.user!.image!} fallback="?" radius='full' size='2' className='cursor-pointer' />
+        </DropdownMenu.Trigger>
 
-              <DropdownMenu.Item>
-                <Link href="/api/auth/signout">Sign Out</Link>
-              </DropdownMenu.Item>
-            </DropdownMenu.Content>
-          </DropdownMenu.Root>
-        }
-        {status === 'unauthenticated' && <Link href="/api/auth/signin" className='m-5'>Sign In</Link>}
-      </Box>
-    </nav>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text>{session!.user?.email}</Text>
+          </DropdownMenu.Label>
+          <Link href="/api/auth/signout">
+          <DropdownMenu.Item>
+            Sign Out
+          </DropdownMenu.Item>
+          </Link>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   )
 }
 
